e6bc2d1aef02e714a38aabad696d0f630fd3aa93
Shiven0504 <120889165+Shiven0504@users.noreply.github.com>
2023-08-20 09:21:45 +0530

    commit

--- calendar_1.py
+++ calendar_1.py
@@ -1,19 +1,5 @@
-class Solution(object):
-    def longestValidSubstring(self, word, forbidden):
-        max_length = 0
-        word_length = len(word)
-
-        for i in range(word_length):
-            for j in range(i+1, word_length+1):
-                substring = word[i:j]
-                if substring in forbidden:
-                    break
-                if len(substring) > max_length:
-                    max_length = len(substring)
-
-        return max_length
-word = "word"
-forbidden = ["for", "bid", "den"]
-solution = Solution()
-result = solution.longestValidSubstring(word, forbidden)
-print(result)
+import calendar
+yy = 2020 #year
+mm = 5 # month
+#display the calendar
+print(calendar.month( yy, mm))
